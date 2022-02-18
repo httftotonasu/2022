2f3019e8ea3ce6275937a46db17b31689f3efc57
HirokiTakano <takano@httf.net>
2022-02-18 16:11:55 +0900

    1st commit

---
+++ stream_main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import time
+
+st.title('Webアプリ入門')
+st.write('プログレスバーの表示')
+'Start'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+'Done!'
+
+left_column,right_column = st.beta_columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('あああ')
+
+expander1 = st.beta_expander('問い合わせ1')
+expander1.write('問い合わせ内容1を書く')
+expander2 = st.beta_expander('問い合わせ2')
+expander2.write('問い合わせ内容2を書く')
+expander3 = st.beta_expander('問い合わせ3')
+expander3.write('問い合わせ内容3を書く')
+
